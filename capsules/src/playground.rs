397b7382f0c18cb569decfb20151040b5b53e15b
Olaf Landsiedel <olaf@cse-31295.cse.chalmers.se>
2018-02-16 17:28:41 +0100

    making playground work

--- capsules/src/playground.rs
+++ capsules/src/playground.rs
@@ -1,11 +1,12 @@
-//! Sample capsule for Tock course at SenSys. It handles an alarm to
-//! sample the ambient light sensor.
+// toggle LEDS in some funny patterns
+// read some sensor
+// wait for button pressed
 
-#![feature(const_fn, const_cell_new)]
-#![no_std]
+//#![feature(const_fn, const_cell_new)]
+//#![no_std]
 
 #[allow(unused_imports)]
-#[macro_use(debug)]
+//#[macro_use(debug)]
 //extern crate kernel;
 
 //use kernel::hil::sensors::{AmbientLight, AmbientLightClient};
@@ -25,12 +26,19 @@ impl<'a, A: Alarm> Playground<'a, A> {
     }
 
     pub fn start(&self) {
-        debug!("Hello World");
+        debug!("Blink Capsule for NRF5x starting");
+        //TODO: need so switch to real time unit
+        //TODO: can we also have reoccuring timers?
+        self.alarm.set_alarm(self.alarm.now().wrapping_add(10000));
     }
 }
 
 impl<'a, A: Alarm> time::Client for Playground<'a, A> {
-    fn fired(&self) {}
+    fn fired(&self) {
+        let t0 = self.alarm.now();
+        self.alarm.set_alarm(t0.wrapping_add(10000));
+        debug!("Alarm fired: {}", t0);
+    }
 }
 
 // impl<'a, A: Alarm> AmbientLightClient for Playground<'a, A> {
